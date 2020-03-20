7af00a9474d55782b44f4338b7a7afcec9c6154e
viperovm <s069362949@gmail.com>
2020-03-20 16:36:22 +0100

    Little more changes

--- lesson2/Singleton/TraitSingleton.php
+++ lesson2/Singleton/TraitSingleton.php
@@ -5,7 +5,7 @@ trait TraitSingleton
 {
     final private function __construct() {}
 
-    public static function getInstance() {
+    final public static function getInstance() {
         if ( empty(self::$instance) ) {
             self::$instance = new self;
         }
